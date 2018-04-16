189aca3feb558f1b670cea6418409f8ab8a22f19
codlab <codlabtech@gmail.com>
2018-04-16 15:48:01 +0200

    remove possible codlab.eu package

--- library/src/androidTest/java/eu/codlab/simplepromise/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/eu/codlab/simplepromise/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("codlab.eu.simplepromise.test", appContext.getPackageName());
+        assertEquals("eu.codlab.simplepromise.test", appContext.getPackageName());
     }
 }
